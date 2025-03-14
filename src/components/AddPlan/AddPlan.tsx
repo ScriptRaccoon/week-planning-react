daf9f61845283bc05e7f99ac635ae2403c4a06b1
Script Raccoon <scriptraccoon@gmail.com>
2025-03-14 10:54:17 +0100

    remove status

--- src/components/AddPlan/AddPlan.tsx
+++ src/components/AddPlan/AddPlan.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import styles from "./AddPlan.module.css"
 
 type Props = {
@@ -6,8 +5,6 @@ type Props = {
 }
 
 export default function AddPlan({ addPlan }: Props) {
-	const [status, setStatus] = useState<string>("")
-
 	function handleSubmit(
 		e: React.FormEvent<HTMLFormElement> & { target: HTMLFormElement }
 	) {
@@ -16,29 +13,21 @@ export default function AddPlan({ addPlan }: Props) {
 		const name = formData.get("name") as string
 		addPlan(name)
 		e.target.reset()
-		setStatus("added")
-		setTimeout(() => setStatus(""), 1000)
 	}
 
 	return (
-		<>
-			<div aria-live='polite' className='sr-only'>
-				{status}
-			</div>
-
-			<form onSubmit={handleSubmit} className={styles.form}>
-				<label className={`label ${styles.inputLabel}`} htmlFor='nameInput'>
-					What do you plan this week?
-				</label>
-				<input
-					type='text'
-					className={`input ${styles.nameInput}`}
-					id='nameInput'
-					name='name'
-					autoComplete='off'
-				/>
-				<button className='button big'>Add</button>
-			</form>
-		</>
+		<form onSubmit={handleSubmit} className={styles.form}>
+			<label className={`label ${styles.inputLabel}`} htmlFor='nameInput'>
+				What do you plan this week?
+			</label>
+			<input
+				type='text'
+				className={`input ${styles.nameInput}`}
+				id='nameInput'
+				name='name'
+				autoComplete='off'
+			/>
+			<button className='button big'>Add</button>
+		</form>
 	)
 }
